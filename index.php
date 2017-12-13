c35ba0dd937de4f9d03785346cea2ca799e64d06
Marie-Axelle Simoes <Vous@exemple.com>
2017-12-13 15:21:39 +0100

    exercice 1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>boucle</title>
+  </head>
+  <body>
+    <?php
+      $yes = true;
+      function hello($yes){
+        return $yes;
+      }
+      echo $yes;
+   ?>
+  </body>
+</html>
